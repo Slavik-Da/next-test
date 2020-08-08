3980ecbaa9e78949487d8ece1cc64492388b5a86
Slavik-Da <61164494+Slavik-Da@users.noreply.github.com>
2020-08-09 01:24:43 +0300

    hooks and loading status added

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,30 @@
-import {useRouter} from "next/router";
+import {useState, useEffect} from 'react'
 import {MainLayout} from "../../components/MainLayout";
 import Link from "next/link";
+import {useRouter} from "next/router";
 
-export default function Post({ post }) {
+export default function Post({ post: serverPost }) {
 
-  //const router = useRouter()
+  const [post, setPost] = useState(serverPost)
+  const router = useRouter()
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`https://simple-blog-api.crew.red/posts/${router.query.id}`)
+      const data = await response.json()
+      setPost(data)
+    }
+    if (!serverPost) {
+      load()
+    }
+  },[])
+
+  if (!post) {
+    return <MainLayout>
+      <p>Loading...</p>
+    </MainLayout>
+  }
+
   return <MainLayout>
     <h1>{post.title}</h1>
     <hr/>
@@ -12,8 +32,11 @@ export default function Post({ post }) {
     <Link href={'/'}><a>Back to latest posts</a></Link>
   </MainLayout>
 }
-Post.getInitialProps = async (ctx) => {
-  const response = await fetch(`https://simple-blog-api.crew.red/posts/${ctx.query.id}`)
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return {post: null}
+  }
+  const response = await fetch(`https://simple-blog-api.crew.red/posts/${query.id}`)
   const post = await response.json()
   return {
     post
